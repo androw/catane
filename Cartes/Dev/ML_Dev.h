1c10317479d6d0c8579926d9a616397611f5b209
Nicolas Lorin <nicolas@androw.eu>
2012-01-09 19:27:02 +0100

    Fixed include

--- Cartes/Dev/ML_Dev.h
+++ Cartes/Dev/ML_Dev.h
@@ -2,11 +2,13 @@
 #define ML_DEV_H
 
 #include "../ML_Carte.h"
-#include "../../Map/ML_Map.h"
 #include "../../ML_Joueur.h"
+
+class ML_Map;
 
 class ML_Dev : public ML_Carte {
 	public:
 		virtual void use(ML_Joueur*, ML_Map*) = 0;
 };
+#include "../../Map/ML_Map.h"
 #endif
